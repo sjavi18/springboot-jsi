881c04bf14537f7c4b2baf599256d53e5eba1297
sjavi18 <sjavi18_alt@hotmail.com>
2025-03-01 20:58:05 -0500

    initial config

---
+++ src/test/java/com/jsi/spring/app1/springboot_jsi/SpringbootJsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsi.spring.app1.springboot_jsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
